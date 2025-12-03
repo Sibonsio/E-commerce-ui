db4f760ddb5cb9bb1ee8c847a025ebed2b7cf34a
Sibonsio <siboniso1sekute@gmail.com>
2025-12-03 09:13:26 +0200

    Carousel : removed useLocation

--- src/componets/Carousel.jsx
+++ src/componets/Carousel.jsx
@@ -4,11 +4,10 @@ import image1 from '../assets/image1.jpeg'
 import image2 from '../assets/image2.jpeg'
 import image3 from '../assets/image3.png'
 import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom'
+
 
 
 const Carousel = () => {
-    const location = useLocation();
     let interval = 3000;
     const images = [image2, image1, image3];
     const [currentIndex, setCurrentIndex] = useState(0);
